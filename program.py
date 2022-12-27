11d32170fe38cbfbf306ab202e8e1019383d9db1
EduardoCarreiro <1211199@isep.ipp.pt>
2022-12-27 03:57:35 +0000

    Cleanup the code

--- program.py
+++ program.py
@@ -2,33 +2,24 @@ import pytube
 import re
 import time
 
-def get_video_title(url):
-    # Create a YouTube object using the URL
-    yt = pytube.YouTube(url)
-
-    # Return the video title
-    return yt.title
-
 def download_video():
-    url=(str)(input('Digite o video a fazer download: '))
-    
-    outputFolder=(str)(input('Digite a pasta do output: '))
-    
-    # Clean up the video title to make it a valid file name
-    title = get_video_title(url)
+    url = input("Enter the video link: ")
+    yt = pytube.YouTube(url)
+    title = yt.title
     title = re.sub(r'[^\w\s-]', '', title)
     title = title.strip().replace(' ', '_')
-    nameOfVideo = f'{title}.mp4'
+    name_of_video = f'{title}.mp4'
     
-    # Create a YouTube object using the URL
-    yt = pytube.YouTube(url)
-
     # Select the highest quality video
     video = yt.streams.get_highest_resolution()
-
+    
+    # Prompt the user to enter the output folder
+    output_folder = input("Enter the output folder: ")
+    
     # Set the output directory and file name
     try:
-        video.download(output_path=outputFolder, filename=nameOfVideo)
+        video.download(output_path=output_folder, filename=name_of_video)
+        print(f'Video downloaded to {output_folder}')
     except Exception as e:
         print(f'An error occurred while downloading the video: {e}')
 
