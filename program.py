096f8dd5b6dd7aaf228ebfc573a17d5868ea4b13
EduardoCarreiro <eduardofcarreiro@gmail.com>
2022-12-27 11:02:43 +0000

    Finally the user can choose to download either mp4 or mp3 and it actually downloads in those formats (previously it would download the mp3 files in webm)

--- program.py
+++ program.py
@@ -3,6 +3,8 @@ import time
 import concurrent.futures
 from tqdm import tqdm
 from pytube import Playlist
+from pytube import YouTube
+from pydub import AudioSegment
 
 def download_video(video, destination_folder):
     # Select the highest resolution stream
@@ -10,8 +12,29 @@ def download_video(video, destination_folder):
 
     # Download the highest resolution stream
     highest_resolution_stream.download(destination_folder)
+    
+def download_video_as_mp3(video, destination_folder):
+    # Get the video's URL using the video_id attribute
+    video_id = video.video_id
+    video_url = f'https://www.youtube.com/watch?v={video_id}'
 
-def download_playlist(playlist_url, destination_folder):
+    # Create a YouTube object using the video's URL
+    yt = YouTube(video_url)
+
+    # Get the first stream and download it to the destination folder
+    video = yt.streams.get_highest_resolution()
+    video.download(destination_folder)
+
+    # Convert the video to an MP3 file using pydub
+    video_path = os.path.join(destination_folder, video.default_filename)
+    audio = AudioSegment.from_file(video_path)
+    mp3_path = os.path.join(destination_folder, video.default_filename.replace('.mp4', '.mp3'))
+    audio.export(mp3_path, format='mp3')
+
+    # Delete the original video file
+    os.remove(video_path)
+
+def download_playlist(playlist_url, destination_folder, file_format):
     # Create the destination folder if it doesn't exist
     if not os.path.exists(destination_folder):
         os.makedirs(destination_folder)
@@ -24,7 +47,10 @@ def download_playlist(playlist_url, destination_folder):
 
     # Use a concurrent.futures.ThreadPoolExecutor to download the videos concurrently
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = [executor.submit(download_video, video, destination_folder) for video in playlist.videos]
+        if file_format == 'mp3':
+            futures = [executor.submit(download_video_as_mp3, video, destination_folder) for video in playlist.videos]
+        elif file_format == 'mp4':
+            futures = [executor.submit(download_video, video, destination_folder) for video in playlist.videos]
 
         # Use tqdm to display a progress bar for the download process
         for future in tqdm(concurrent.futures.as_completed(futures), total=len(futures)):
@@ -39,7 +65,11 @@ def run_download_playlist():
     # Prompt the user for the destination folder
     destination_folder = input('Enter the destination folder for the downloaded videos: ')
 
-    download_playlist(playlist_url, destination_folder)
+    # Prompt the user to choose between MP4 or MP3 files
+    file_format = input('Enter the desired file format (MP4 or MP3): ')
+
+    download_playlist(playlist_url, destination_folder, file_format)
+
 
 run_download_playlist()
 
